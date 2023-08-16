6425c5e99d7276d7719553ee886e2be2d616791b
BlutzerZ <wafiqnurudin@gmail.com>
2023-08-16 20:21:19 +0700

    base

---
+++ routes/UserRoutes.go
@@ -0,0 +1,10 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func UserRoutes(r *gin.Engine) {
+	// r.GET("/user", controllers.GetAllUser)
+	// r.GET("/user/{id}", controllers.GetUserByID)
+	// r.POST("/user", controllers.CreateUser)
+	// r.PUT("/user", controllers.EditUser)
+}
